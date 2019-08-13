6ec6f6fe7acf94b4aa39bc91e460d58c2047729d
nicola <nicola@nicolas-Mac-mini.local>
2019-08-13 09:33:20 +0100

    test app and traefik

--- kubernetes/scripts/join-node.sh
+++ kubernetes/scripts/join-node.sh
@@ -2,8 +2,5 @@ join_command=$1
 if [[  $(pgrep kubelet) ]]; then 
   echo "Kubernetes is already running on this node...skipping."
 else
-  # Configure sysctl
-  # sudo echo "net.bridge.bridge-nf-call-iptables = 1" >>  /etc/sysctl.conf 
-  # sudo sysctl -p >> node.log 2>&1
   eval $join_command
 fi                    
--- templates/cloud-init.sh
+++ templates/cloud-init.sh
@@ -10,14 +10,54 @@ yum remove \
     docker-logrotate \
     docker-engine
 
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+
+# Setup required sysctl params, these persist across reboots.
+cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+#======================================================#
+#              Install Docker
+#======================================================#
 yum update -y
 yum install -y amazon-linux-extras yum-utils device-mapper-persistent-data lvm2 tc
 amazon-linux-extras install -y docker
+
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
 systemctl enable docker
 systemctl start docker
 usermod -aG docker ec2-user
 
 
+#======================================================#
+#              Install K8s prerequisistes
+#======================================================#
 #Add K8s repo
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -29,9 +69,7 @@ repo_gpgcheck=0
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
-# Set SELinux in permissive mode (effectively disabling it)
-setenforce 0
-sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
 
 # Install kubelet and kubectl
 yum install -y kubeadm-${kubernetes_version}-0.x86_64 kubelet-${kubernetes_version}-0.x86_64 kubectl-${kubernetes_version}-0.x86_64 --disableexcludes=kubernetes
